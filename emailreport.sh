a3eed8121d3d4df216b0003c41b5657970456f2d
Jason Blanchard <jas.blanchard@gmail.com>
2011-12-13 18:36:50 -0500

    Modified README and added comments in the scripts

--- emailreport.sh
+++ emailreport.sh
@@ -9,5 +9,6 @@ email=$1
 
 if [ ~/reports/${date}_report ]
 then
+	# Jason is my name, but you can change it here. It will appear in the subject line.
 	mail -s "Jason's Weekly Report - $week" $1 < ~/reports/${date}_report
 fi
--- wk.sh
+++ wk.sh
@@ -8,6 +8,7 @@ task=$@
 
 if [ ! -e ~/reports/${date}_report ]
 then
+	# My name is Jason, but you can enter your name here. It will appear at the top of the email
 	echo "Jason's Weekly Report - $week" >> ~/reports/${date}_report
 	echo "" >> ~/reports/${date}_report
 fi
